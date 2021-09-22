f6750b55d93835594c99cb8a6ec12659bfe36354
Sahar <saharmansouri19@gmail.com>
2021-09-22 20:16:47 +0330

    bog fixed.

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-function element() {
-
-}
-
-const input = ducument.getElementById(element: "range")
+const input = document.getElementById("range")
 const min = input.min
 const max= input.max
 const value = input.value
 
 //
-input.style.background = 'linear-gradient(to right,red 0%, red ${(value-min)/(max-min)*100}%), blue 100%)'
+input.style.background =`linear-gradient(to right,red 0%, red ${(value-min)/(max-min)*100}%, blue 100%)`
 
 input.oninput = function (){
-    this.style.background ='linear-gradient(to right,red 0%, blue ${(this.value-this.min)/(this.max-this.min)*100%, blue 100%)'
-}
+    this.style.background =`linear-gradient(to right,red 0%, red ${(this.value-this.min)/(this.max-this.min)*100}%, blue 100%)`
+    }
